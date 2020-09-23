0dd1dba7070103a6da3353f8e423ed97e175fe7a
medkov77 <69788617+medkov77@users.noreply.github.com>
2020-09-23 19:42:56 +0300

    Update main.js

--- clean/js/main.js
+++ clean/js/main.js
@@ -15,8 +15,8 @@ $('.servis-slider').slick({
       slidesToScroll: 1,
       centerMode: true,
       centerPadding: '0px',
-      prevArrow: "<img src='../images/servis/prev.png' class='prev' alt='1'>",
-      nextArrow: "<img src='../images/servis/next.png' class='next' alt='2'>",
+      prevArrow: "<img src='/images/servis/prev.png' class='prev' alt='1'>",
+      nextArrow: "<img src='/images/servis/next.png' class='next' alt='2'>",
     		speed:1000,
 		autoplaySpeed:800,
 		responsive: [
