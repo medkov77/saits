3e0e32c2a4164efe4fcd628f1f5f2918ce021ae5
medkov77 <69788617+medkov77@users.noreply.github.com>
2020-09-27 20:11:27 +0300

    Add files via upload

---
+++ Hosting/js/main.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	
+	$('.accordion__triger').click(function (){
+    	$(this).parent().toggleClass('active');
+     
+	});
+ $('.slider').slick({
+  arrows:false,
+  dots:false,
+  autoplay:false,
+  slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: false,
+      centerPadding: '0px',
+    speed:1000,
+    autoplaySpeed:800,
+     asNavFor: '.text-slider',
+  lazyLoad: 'ondemand',
+  responsive: [
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ],
+  customPaging:function(){return ''}
+})
+$('.text-slider').slick({
+  arrows:true,
+   prevArrow: "<img src='../images/ic_outline-navigate-prev.svg' class='prev' alt='1'>",
+   nextArrow: "<img src='../images/ic_outline-navigate-next.svg' class='next' alt='2'>",
+   dots:false,
+    autoplay:false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed:1000,
+    autoplaySpeed:800,
+    centerMode: false,
+    centerPadding: '0px',
+    asNavFor: '.slider',
+    lazyLoad: 'ondemand',
+  customPaging:function(){return ''}
+})
+  $('.burger').click(function(event) {
+    $('.burger,.menu,.nav,.btn').toggleClass('active');
+  });
+  $(".arrow").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1000);
+    return false;
+  });
+
+});
